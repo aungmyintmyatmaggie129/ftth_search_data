5a4e8fb6b9cc242ec8153f9ce97cc3c644dfb2f7
VMY016969 <VMY016969@MYTEL.COM.MM>
2026-09-23 15:31:08 +0630

    fix server issue

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,21 +7,23 @@ const path = require('path');
 const customerRoutes = require('./routes/customers');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/customers', customerRoutes);
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
-    process.exit(1);
   });
+
+module.exports = app;
